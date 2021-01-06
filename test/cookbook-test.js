0d24ac2afa3cc3f202fd18ee9ab779441bcbbff5
kellydinneen <kgdinneen@gmail.com>
2021-01-06 10:28:02 -0700

    Show search bar

--- test/cookbook-test.js
+++ test/cookbook-test.js
@@ -18,22 +18,20 @@ describe('Cookbook', () => {
     expect(cookbook.recipes[1].name).to.equal("Pumpkin Cheesecake Breakfast Smoothie");
   });
 
-  describe('findRecipe', () => {
-    it('Should filter recipes by ingredients', () => {
-      expect(cookbook.findRecipes('milk').length).to.equal(2);
-      expect(cookbook.findRecipes('almond').length).to.equal(1);
-      expect(cookbook.findRecipes('blue').length).to.equal(0);
-    });
+  it('Should filter recipes by ingredients', () => {
+    expect(cookbook.findRecipes('milk').length).to.equal(2);
+    expect(cookbook.findRecipes('almond').length).to.equal(1);
+    expect(cookbook.findRecipes('blue').length).to.equal(0);
+  });
 
-    it('Should filter recipes by name', () => {
-      expect(cookbook.findRecipes('Macaroni').length).to.equal(1);
-      expect(cookbook.findRecipes('Sesame Cookies').length).to.equal(0);
-    });
+  it('Should filter recipes by name', () => {
+    expect(cookbook.findRecipes('Macaroni').length).to.equal(1);
+    expect(cookbook.findRecipes('Sesame Cookies').length).to.equal(0);
+  });
 
-    it('Should filter recipes by tag', () => {
-      expect(cookbook.findRecipes('dinner').length).to.equal(1);
-      expect(cookbook.findRecipes('comfort').length).to.equal(2);
-      expect(cookbook.findRecipes('rasberry').length).to.equal(0);
-    });
+  it('Should filter recipes by tag', () => {
+    expect(cookbook.findRecipes('dinner').length).to.equal(1);
+    expect(cookbook.findRecipes('comfort').length).to.equal(2);
+    expect(cookbook.findRecipes('rasberry').length).to.equal(0);
   });
 })
