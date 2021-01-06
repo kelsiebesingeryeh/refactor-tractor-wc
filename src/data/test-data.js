f7f1480511f9ce5a1c4ba62623b627896ce40e08
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-01-05 17:34:56 -0700

    Add test data in test-file.js

---
+++ src/data/test-data.js
@@ -0,0 +1,154 @@
+let testUsers = [{
+    "id": 500,
+    "name": "Marty McFly",
+    "pantry": [{
+            "ingredient": 5555,
+            "amount": 2
+        },
+        {
+            "ingredient": 1145,
+            "amount": 1
+        },
+        {
+            "ingredient": 1111,
+            "amount": 3
+        },
+        {
+            "ingredient": 2047,
+            "amount": 5
+        },
+        {
+            "ingredient": 11220,
+            "amount": 4
+        },
+        {
+            "ingredient": 777,
+            "amount": 1
+        },
+        {
+            "ingredient": 1919,
+            "amount": 3
+        }
+    ]
+}]
+
+let testRecipeData = [{
+    "name": "Simple Macaroni and Cheese",
+    "id": 595736,
+    "image": "https://spoonacular.com/recipeImages/595736-556x370.jpg",
+    "ingredients": [{
+            "name": "elbow macaroni",
+            "id": 5555,
+            "quantity": {
+                "amount": 8,
+                "unit": "oz"
+            }
+        },
+        {
+            "name": "butter",
+            "id": 1145,
+            "quantity": {
+                "amount": .25,
+                "unit": "c"
+            }
+        },
+        {
+            "name": "all purpose flour",
+            "id": 1111,
+            "quantity": {
+                "amount": .25,
+                "unit": "c"
+            }
+        },
+        {
+            "name": "salt",
+            "id": 2047,
+            "quantity": {
+                "amount": 0.5,
+                "unit": "tsp"
+            }
+        },
+        {
+            "name": "ground black pepper",
+            "id": 11220,
+            "quantity": {
+                "amount": 0.5,
+                "unit": "tsp"
+            }
+        },
+        {
+            "name": "milk",
+            "id": 777,
+            "quantity": {
+                "amount": 1,
+                "unit": "c"
+            }
+        } {
+            "name": "shredded cheddar cheese",
+            "id": 1919,
+            "quantity": {
+                "amount": 2,
+                "unit": "c"
+            }
+        },
+    ],
+    "instructions": [{
+            "number": 1,
+            "instruction": "Bring a large pot of lightly salted water to a boil. Cook elbow macaroni in the boiling water, stirring occasionally until cooked through but firm to the bite, 8 minutes. Drain."
+        },
+        {
+            "number": 2,
+            "instruction": "Melt butter in a saucepan over medium heat; stir in flour, salt, and pepper until smooth, about 5 minutes. Slowly pour milk into butter-flour mixture while continuously stirring until mixture is smooth and bubbling, about 5 minutes. Add Cheddar cheese to milk mixture and stir until cheese is melted, 2 to 4 minutes."
+        },
+        {
+            "number": 3,
+            "instruction": "Fold macaroni into cheese sauce until coated."
+        },
+    ],
+    "tags": [
+        "cheese",
+        "dinner",
+        "pasta",
+        "comfort food",
+    ]
+}]
+
+let testIngredientsData = [{
+        {
+            "id": 5555,
+            "name": "elbow macaroni",
+            "estimatedCostInCents": 580
+        },
+        {
+            "id": 1145,
+            "name": "butter",
+            "estimatedCostInCents": 617
+        },
+        {
+            "id": 1111,
+            "name": "all purpose flour",
+            "estimatedCostInCents": 205
+        },
+        {
+            "id": 2047,
+            "name": "salt",
+            "estimatedCostInCents": 100
+        },
+        {
+            "id": 11220,
+            "name": "ground black pepper",
+            "estimatedCostInCents": 130
+        },
+        {
+            "id": 7777,
+            "name": "milk",
+            "estimatedCostInCents": 305
+        },
+        {
+            "id": 1919,
+            "name": "shredded cheddar cheese",
+            "estimatedCostInCents": 555
+        },
+    }
+
+]
